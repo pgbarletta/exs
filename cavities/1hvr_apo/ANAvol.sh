2a138cfe2ff965052c765ff6c1d93e5f6694c5de
pgbarletta <pbarletta@gmail.com>
2016-11-05 16:05:14 -0300

    Added analysis for both 1hvr_apo and holo. MD analysis for both of them too.

---
+++ cavities/1hvr_apo/ANAvol.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA ../bu/avg_1hvr_apo.pdb -c ../bu/1hvr_apo.cfg -d ../../run/1hvr_apo/pca/fit_1hvr_apo.nc -o whole_1hvr_apo > vol_whole_1hvr_apo
+
+exit 0
---
+++ cavities/1hvr_hol/ANAvol.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA ../bu/apoavg_1hvr_hol.pdb -c ../bu/1hvr_hol.cfg -d ../../run/1hvr_hol/pca/fit_1hvr_hol.nc -o whole_1hvr_hol > vol_whole_1hvr_hol
+
+exit 0
