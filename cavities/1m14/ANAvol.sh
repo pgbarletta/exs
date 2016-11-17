d47e06ce55d8b52d13d5e49fa12237cc193b5494
pgbarletta <pbarletta@gmail.com>
2016-11-17 01:11:38 -0300

    Added EGFR analysis

---
+++ cavities/1m14/ANAvol.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA ../bu/avg_1m14.pdb -c ../bu/1m14.cfg -d ../../run/1m14/pca/fit_1m14.nc -o whole_1m14 > vol_whole_1m14
+
+exit 0
---
+++ cavities/1xkk/ANAvol.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA ../bu/avg_1xkk.pdb -c ../bu/1xkk.cfg -d ../../run/1xkk/pca/fit_1xkk.nc -o whole_1xkk > vol_whole_1xkk
+
+exit 0
