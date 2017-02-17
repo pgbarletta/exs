20dcf6bfa02e26b9d276e0254b447738ce0209d1
pgbarletta <pbarletta@gmail.com>
2017-02-17 09:18:47 -0300

    Added publication quality plots

---
+++ paper/4-fig/porc_1hvr_apo.py
@@ -0,0 +1,17 @@
+from pymol import cmd
+
+cmd.load("modevectors.py")
+cmd.load("./avg_1hvr_apo.pdb")
+cmd.load("disp_avg_1hvr_apo.pdb")
+
+cmd.set_view ( '''-0.554374218,    0.354257464,    0.753099322,\
+    -0.799625099,    0.024184536,   -0.599998593,\
+    -0.230775893,   -0.934826553,    0.269876897,\
+     0.000570767,   -0.000263005, -126.341300964,\
+    44.034534454,   42.536033630,   34.025039673,\
+   -99.837615967,  352.591064453,  -20.000000000''' )
+
+
+modevectors("avg_1hvr_apo", "disp_avg_1hvr_apo", head=.7, tail=0.2, head_length=1.2, headrgb=".25,.5,.8", tailrgb=".25,.5,.8", cutoff=1.5)
+
+cmd.png("4a-porc_1hvr_apo.png", width=3600, height=2300, dpi=600, ray=1)
---
+++ paper/4-fig/porc_256l.py
@@ -0,0 +1,18 @@
+from pymol import cmd
+
+cmd.load("modevectors.py")
+cmd.load("./cut_avg_256l.pdb")
+cmd.load("disp_avg_256l.pdb")
+
+cmd.set_view ( '''0.315200627,   -0.223496795,    0.922329843,\
+     0.930618227,    0.263243794,   -0.254244953,\
+    -0.185979143,    0.938482106,    0.290963858,\
+    -0.000527410,   -0.000355184, -100.719245911,\
+    32.342666626,   35.357192993,   38.785629272,\
+  -274.016784668,  475.400878906,  -20.000000000''' )
+
+modevectors("cut_avg_256l", "disp_avg_256l", head=.7, tail=0.2, head_length=1.2, headrgb=".25,.5,.8", tailrgb=".25,.5,.8", cutoff=2)
+
+cmd.hide('''cartoon''', '''resi 59+60+61+92+93+94+133+134''' )
+
+cmd.png("4d-porc_256l.png", width=3600, height=2300, dpi=600, ray=1)
